ed1e43882c4c9de435f8b59170fbf55754b3558b
Windows <patriciafaccio17@gmail.com>
2025-06-01 17:55:30 -0300

    Level 2 implementation

--- code/EntityFactory.py
+++ code/EntityFactory.py
@@ -14,9 +14,15 @@ class EntityFactory:
         match entity_name:
             case 'Level1Bg':
                 list_bg = []
-                for i in range(7):
-                    list_bg.append(Background(f'Level1Bg{i}', (0,0)))
+                for i in range(7):  # número de imagens level1
+                    list_bg.append(Background(f'Level1Bg{i}', (0, 0)))
                     list_bg.append(Background(f'Level1Bg{i}', (WIN_WIDTH, 0)))
+                return list_bg
+            case 'Level2Bg':
+                list_bg = []
+                for i in range(5):  # número de imagens do level2
+                    list_bg.append(Background(f'Level2Bg{i}', (0, 0)))
+                    list_bg.append(Background(f'Level2Bg{i}', (WIN_WIDTH, 0)))
                 return list_bg
             case 'Player1':
                 return Player('Player1', (10, WIN_HEIGHT / 2 - 30))
